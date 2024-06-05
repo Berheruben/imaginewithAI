629307522413d651e1808d248707dd294fd3662d
Berheruben <rubenberhe56@yourdomain.com>
2024-06-05 17:14:48 +0200

    fix import

--- app/(root)/credits/page.tsx
+++ app/(root)/credits/page.tsx
@@ -2,11 +2,12 @@ import { SignedIn, auth } from "@clerk/nextjs";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
-import Header from "@/components/shared/Header";
-import { Button } from "@/components/ui/button";
-import { plans } from "@/constants";
-import { getUserById } from "@/lib/actions/user.actions";
-import Checkout from "@/components/shared/Checkout";
+import Header from "../../../components/shared/Header";
+import { Button } from "../../../components/ui/button";
+import { plans } from "../../../constants";
+import { getUserById } from "../../../lib/actions/user.actions";
+import Checkout from "../../../components/shared/Checkout";
+import React from "react";
 
 const Credits = async () => {
   const { userId } = auth();
